9b78d5617c736c8065b388444940dda46663c747
Alexey <ale-kuchner@mail.ru>
2022-03-31 23:18:55 +0500

    first commit

--- src/main/java/ru/learnUp/feb/MultiplyProcessor.java
+++
@@ -1,9 +0,0 @@
-package ru.learnUp.feb;
-
-public class MultiplyProcessor {
-
-    public int process(int arg1, int arg2) {
-        int multiply = arg1 * arg2;
-        return multiply;
-    }
-}
--- src/main/java/ru/learnUp/feb/SumProcessor.java
+++
@@ -1,9 +0,0 @@
-package ru.learnUp.feb;
-
-public class SumProcessor {
-
-    public int process(int arg1, int arg2) {
-        int sum = arg1 + arg2;
-        return sum;
-    }
-}
